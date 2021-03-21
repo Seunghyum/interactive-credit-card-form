1c11a93052f01bc4e4f45350c9dccfd8f0a161bb
seunghyum <antmgu@gmail.com>
2021-03-21 23:53:44 +0900

    chore: cleanWebpackPlugin setting from common to prod

--- build/webpack.config.prod.js
+++ build/webpack.config.prod.js
@@ -2,16 +2,19 @@ const { merge } = require('webpack-merge');
 const { HotModuleReplacementPlugin } = require('webpack');
 const baseConfig = require('./webpack.config.common');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const CompressionPlugin = require("compression-webpack-plugin");
+const CompressionPlugin = require('compression-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = merge(baseConfig, {
   mode: 'production',
+  devtool: 'cheap-module-source-map',
   output: {
     publicPath: './',
   },
   plugins: [
-    new HotModuleReplacementPlugin(), 
+    new HotModuleReplacementPlugin(),
     new BundleAnalyzerPlugin(),
-    new CompressionPlugin()
+    new CompressionPlugin(),
+    new CleanWebpackPlugin(),
   ],
 });
