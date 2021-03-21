534b805b0b939ead80aabd4ba5d2d863e4956b8a
seunghyum <antmgu@gmail.com>
2021-03-21 23:44:41 +0900

    chore: add postcss loader and setting

--- build/webpack.config.common.js
+++ build/webpack.config.common.js
@@ -33,6 +33,7 @@ module.exports = {
         use: [
           'style-loader',
           'css-loader',
+          'postcss-loader',
           'sass-loader'
         ],
         exclude: /node_modules/,
