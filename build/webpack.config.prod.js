3fce52e167cb2b83d3c16d995734a440756fe2f4
seunghyum <antmgu@gmail.com>
2021-03-21 23:46:50 +0900

    chore: add wepback-bundle-analyzer

--- build/webpack.config.prod.js
+++ build/webpack.config.prod.js
@@ -1,11 +1,15 @@
 const { merge } = require('webpack-merge');
 const { HotModuleReplacementPlugin } = require('webpack');
 const baseConfig = require('./webpack.config.common');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = merge(baseConfig, {
   mode: 'production',
   output: {
     publicPath: './',
   },
-  plugins: [new HotModuleReplacementPlugin()],
+  plugins: [
+    new HotModuleReplacementPlugin(), 
+    new BundleAnalyzerPlugin()
+  ],
 });
